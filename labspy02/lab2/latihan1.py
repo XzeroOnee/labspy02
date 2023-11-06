975191bf910c2e492e7f40bc31a772118f4b0c30
XzeroOnee <147376877+XzeroOnee@users.noreply.github.com>
2023-11-06 12:42:37 +0700

    Add files via upload

---
+++ labspy02/lab2/latihan1.py
@@ -0,0 +1,14 @@
+#Agus Setiawan
+#312310597
+#TI.23.A6
+
+bilangan1 = float(input("Masukkan bilangan pertama: "))
+bilangan2 = float(input("Masukkan bilangan kedua: "))
+
+
+if bilangan1 > bilangan2:
+    print(f"Bilangan terbesar adalah: {bilangan1}")
+elif bilangan2 > bilangan1:
+    print(f"Bilangan terbesar adalah: {bilangan2}")
+else:
+    print("Kedua bilangan sama besar.")
