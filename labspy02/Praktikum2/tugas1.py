bd213a3fabf4afe224fbab597c3fe593e29defbf
XzeroOnee <147376877+XzeroOnee@users.noreply.github.com>
2023-11-06 17:44:27 +0700

    Add files via upload

---
+++ labspy02/Praktikum2/tugas1.py
@@ -0,0 +1,18 @@
+#Agus Setiawan
+#312310597
+#TI.23.A6
+
+a = int (input("masukan bilangan A:"))
+b = int (input("masukan bilangan B :"))
+c = int (input("masukan bilangan C :"))
+
+if a > b and a > c :
+    print("A adalah bilangan terbesar")
+
+elif b > a and b > c :
+     print("B adalah bilangan terbesar")
+
+else :
+    print("C adalah bilangan terbesar")
+
+
